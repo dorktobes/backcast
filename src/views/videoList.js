aa4eb291bb31891f9a7d5301502e773e99192538
dorktobes <t.hoshman@gmail.com>
2017-10-23 20:07:04 -0700

    made some progress

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,8 +1,20 @@
 var VideoListView = Backbone.View.extend({
 
+  el: '.list',
+  
+  initialize: function () {
+    this.videos = new Videos(exampleVideoData);
+    console.log(this.videos);
+    this.render();
+  },
 
   render: function() {
     this.$el.children().detach();
+    this.videos.forEach(function (videoElement) {
+      console.log('here is video element: ', videoElement);
+      var currentView = new VideoListEntryView(videoElement);
+      // this.$el.append(currentView);
+    });
     this.$el.html(this.template());
     return this;
   },
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,5 +1,10 @@
 var VideoListEntryView = Backbone.View.extend({
-
+  // el: '.video-list',
+  
+  initialize: function () {
+    // this.model.on('change', this.render, this);
+    this.render();
+  },
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
